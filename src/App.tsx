53f7b3de66d4bc14955c634f14cfd4bb90f7e7b4
kidow <wcgo2ling@hanmail.net>
2021-04-05 01:00:38 +0900

    0.0.7 useObject hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
+import { useObject } from 'services'
 
-function App() {
-  return <div>Anythink Start!</div>
+export interface Props {}
+interface State {
+  content: string
+}
+
+const App: FunctionComponent<Props> = () => {
+  const [{ content }, setState] = useObject<State>({ content: '' })
+  return <div className="container mx-auto">App</div>
 }
 
 export default App
