9de7d8f46832380f050f78d23ad5bb0f6f8ba720
kidow <wcgo2ling@hanmail.net>
2021-04-04 23:17:54 +0900

    0.0.5 firebase 세팅

--- src/types/node.d.ts
+++ src/types/node.d.ts
@@ -2,5 +2,12 @@ declare namespace NodeJS {
   interface Process {
     env: ProcessEnv
   }
-  interface ProcessEnv {}
+  interface ProcessEnv {
+    REACT_APP_API_KEY: string
+    REACT_APP_AUTH_DOMAIN: string
+    REACT_APP_PROJECT_ID: string
+    REACT_APP_STORAGE_BUCKET: string
+    REACT_APP_APP_ID: string
+    REACT_APP_MEASUREMENT_ID: string
+  }
 }
